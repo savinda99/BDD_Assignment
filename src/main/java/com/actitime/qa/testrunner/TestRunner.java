dab905987ac0ba51d0b94a5a9ba8266bda4e3f86
s.kadigamugedara <s.kadigamugedara@exactprosystems.com>
2023-05-31 11:45:13 +0530

    first commit

---
+++ src/main/java/com/actitime/qa/testrunner/TestRunner.java
@@ -0,0 +1,27 @@
+package com.actitime.qa.testrunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "C:\\Users\\s.kadigamugedara_exa\\Documents\\Uni\\BDD_Demo-main\\src\\main\\java\\com\\actitime\\qa\\feature\\Loging.feature"
+		,glue={"com/actitime/qa/stepdefinitions"}
+		,monochrome = true,
+				strict=true,
+				dryRun=false
+				
+		)
+
+
+
+
+public class TestRunner {
+	
+	
+	
+
+}
